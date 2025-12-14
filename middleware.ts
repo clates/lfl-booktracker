02c0794f69f5c18ac2435c80ce230d06dac4ae15
Christopher Lates <ChristopherLates@gmail.com>
2025-12-14 16:09:05 -0500

    Update

--- middleware.ts
+++ middleware.ts
@@ -8,3 +8,15 @@ export async function middleware(req: NextRequest) {
   await supabase.auth.getSession();
   return res;
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+};
